18c70e46b546c5c9d487bae4c40daa2131273f47
Tovarasul <tonyfrum@yahoo.com>
2018-07-14 19:35:23 +0300

    am adaugat 2 prefaburi jumper si pusher, le puteti duplica si sa faceti un lvl, enjoy :D

--- Tony's workspace/Ball Game/Assets/Scripts/PlayerController.cs
+++ Tony's workspace/Ball Game/Assets/Scripts/PlayerController.cs
@@ -8,42 +8,69 @@ public class PlayerController : MonoBehaviour {
     public float speed;
     public Text countText;
     public Text winText;
+    public float jumper;
 
     private Rigidbody rb;
     private int count;
 
     void Start()
     {
+        Debug.Log(Physics.gravity.x + " " + Physics.gravity.y + " " + Physics.gravity.z);
+        //Physics.gravity = new Vector3(0, -500.0F, 0);
         rb = GetComponent<Rigidbody>();
         count = 0;
         SetCountText();
         winText.text = "";
     }
-        
-	void FixedUpdate()
+
+    void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal,0.0f, moveVertical);
-        if(!rb)
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        if (!rb)
         {
             Debug.Log("naspa");
         }
         else
         {
-            Debug.Log("ok");
-            rb.AddForce(movement*speed);
+            //Debug.Log("ok");
+            rb.AddForce(movement * speed);
         }
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Pick Up"))
+        if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
             ++count;
             SetCountText();
+        }
+        else if (other.gameObject.CompareTag("EndOfMap"))
+        {
+            transform.position = new Vector3(0.0f, 1.5f, 0.0f);
+        }
+        /*else if(other.gameObject.CompareTag("Wall_Test"))
+        {
+            Debug.Log("perete");
+            float moveHorizontal = -Input.GetAxis("Horizontal");
+            float moveVertical = -Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+            rb.AddForce(movement * speed / 2);
+        }*/
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("perete333");
+        if (collision.gameObject.CompareTag("Jumper"))
+        {
+            Debug.Log("perete222");
+            Vector3 movement = new Vector3(0f, jumper, 0f);
+            rb.AddForce(movement*10);
         }
     }
 
---
+++ Tony's workspace/Ball Game/Assets/Scripts/Pusher.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pusher : MonoBehaviour {
+
+    public float time;
+    public float speed;
+    private bool tempBool = true;
+    private float tempFloat = 0;
+
+    private float initX;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tempFloat += Time.deltaTime;
+        //Debug.Log(tempBool);
+        if (tempBool == true && tempFloat > time)
+        {
+            tempBool = false;
+            tempFloat -= time;
+        }
+        else if (tempBool == false && tempFloat > time*2)
+        {
+            tempBool = true;
+            tempFloat -= time*2;
+        }
+
+        if (tempBool == true)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * 2*speed);
+        }
+        else
+        {
+            transform.Translate(Vector3.right * Time.deltaTime*speed);
+        }
+    }
+}
